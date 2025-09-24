9417aa6a3daa7e07896d9813c8f085c498a03b5f
SyukurGit <220705058@student.ar-raniry.ac.id>
2025-09-24 13:15:35 +0700

    Pesan update

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'keycloak_id', // Tambahkan ini
+    'avatar',      // Tambahkan ini
+    'kc_payload',  // Tambahkan ini
     ];
 
     /**
